a44369eff2c2284b6deb1b28e23fcc99ba622902
kulbirmalik <kulbirmalik1999@gmail.com>
2025-05-14 02:31:13 +0530

    removed testcase

--- src/test/java/org/gitter/repository/GitterRepositoryTest.java
+++ src/test/java/org/gitter/repository/GitterRepositoryTest.java
@@ -55,19 +55,4 @@ class GitterRepositoryTest {
         List<GitterCommitEntry> history = gitterRepository.getCommitHistory();
         assertEquals(1, history.size());
     }
-
-    @Test
-    void testDetectModifiedFiles(@TempDir File tempDir) throws IOException {
-        File gitterDir = new File(tempDir, ".gitter");
-        assertTrue(gitterDir.mkdir());
-
-        File file = new File(gitterDir, "file1.txt");
-        Files.writeString(file.toPath(), "initial content");
-
-        gitterRepository.markFileAsCommitted("file1.txt", "initial content");
-        Files.writeString(file.toPath(), "updated content");
-
-        List<String> modified = gitterRepository.detectModifiedFiles();
-        assertTrue(modified.contains("file1.txt"));
-    }
 }
